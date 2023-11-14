447a36be3eef0e8c0a65a4874a69c566ace86e74
Raghul <84333311+raghulkumar28@users.noreply.github.com>
2023-11-14 10:25:01 +0530

    server.js

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const http = require('http');
 const WebSocket = require('ws');
 
 const port = 6969;
-const server = http.createServer(express);
-const wss = new WebSocket.Server({ server })
+const app = express(); // Create an express app
+const server = http.createServer(app); // Pass the express app to createServer
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
@@ -12,10 +13,10 @@ wss.on('connection', function connection(ws) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
       }
-    })
-  })
-})
+    });
+  });
+});
 
-server.listen(port, function() {
-  console.log(`Server is listening on ${port}!`)
-})
+server.listen(port, function () {
+  console.log(`Server is listening on ${port}!`);
+});
